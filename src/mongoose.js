9ed47443efba01d28ff6e226ffdf4565846ea79b
Karen Vardumyan <karenvardumyan94@gmail.com>
2020-01-18 22:09:43 +0400

    add database connection error handling

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/node_project', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/node_project', { useNewUrlParser: true })
+    .then((result) => {
+        console.log('Connection to database completed successfully!');
+    })
+    .catch((error) => {
+        console.log('Could not connect to database!');
+    })
 
 module.exports = mongoose;
